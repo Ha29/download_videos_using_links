57e15865266ec232a90799a42f25d1366dff4086
Steven Ha <stevenha@berkeley.edu>
2020-12-08 02:05:29 +0000

    debug process to run incrementally

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # download_videos_using_links
 Scraped all video id's from a specific channel using youtube-dl, Youtube Data API, a cloud server, and python. 
 
-The purpose of downloading the videos from this channel was to make sure the channel would not be taken down given that the account was no longer accessible.
+The purpose of downloading the videos from this channel was to make sure the channel would not be taken down given that the account was given warning that it was going to be no longer accessible. The purpose of usage of the cloud server was to be able to download all videos without receiving the 429 HTTP error from Youtube's servers by using the youtube-dl command tool. 
 
 Transfered files on the cloud via scp and ssh to run the script, used python3.8, used screen to ensure script process could easily be checked.
+
+The exclude.txt file was later added due to space limitations on the cloud server.
+
+###
+INSTALLATION
+###
+
+pip3 install termcolor
--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import json, os, re
-
+from termcolor import colored
 
 def fix_characters(title):
   return re.sub('[^0-9a-zA-Z]+', ' ', title)
@@ -18,19 +18,31 @@ def load_video_data(fpath):
     videos += append_videos(d1)
   return videos
 
+def in_excluded_list(title):
+  excluded_list = open('exclude.txt', 'r')
+  vids = [line.strip() for line in excluded_list.readlines()]
+  if title + ".mkv" in vids or title + ".mp4" in vids:
+    return True
+  return False
+
 def download_videos():
   videos = []
   videos += load_video_data("videos1.json") + load_video_data("videos2.json")
+  vids_downloaded = 0
   for video_id, title in videos:
-    print("youtube-dl -o " + title.replace(' ', '_').replace('(', '') + " " + video_id)
+    if vids_downloaded > 5:
+        break
+    title = title.replace(' ', '_')
     mkv_path = "videos/" + title + ".mkv"
     mp4_path = "videos/" + title + ".mp4"
-    download_fpath = "videos/" + title.replace(' ', '_')
-    if not os.path.isfile(mkv_path) and not os.path.isfile(mp4_path):
-      print(video_id + " downloading: " + download_fpath)
-      os.system("youtube-dl -o " + download_fpath + " " + video_id)
+    download_fpath = "videos/" + title
+    if not in_excluded_list(title) and not os.path.isfile(mkv_path) and not os.path.isfile(mp4_path):
+      print(colored(video_id + " downloading: " + download_fpath, "green"))
+      print(colored("youtube-dl -o " + download_fpath + " " + video_id, "green"))
+      # os.system("youtube-dl -o " + download_fpath + " " + video_id)
+      vids_downloaded += 1
     else:
-      print("skipping download: " + title + "with youtube_id: " + video_id)
+      print(colored("skipping download: " + title + "with youtube_id: " + video_id, "yellow"))
 
 if __name__ == "__main__":
   download_videos()
