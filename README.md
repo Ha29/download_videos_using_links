11d7f32ab49bf30cb39fa81f378dc60794ed469c
Steven Ha <stevenha@berkeley.edu>
2020-12-08 18:45:12 +0000

    add args options and helper text

--- README.md
+++ README.md
@@ -33,6 +33,19 @@ You need a videos/ directory in order to properly download the videos.
 Due to memory limitations for the cloud server used to originally use this script, the script downloads only 6 videos at a time. The exclude.txt file is used to exclude video titles that have already been downloaded. Each line is the name of the file that is already downloaded. The code will skip downloading these videos. 
 
 ###
+LIMITATIONS
+###
+The current script does not quit execution if the device that downloads are stored runs out of memory. YoutubeDL sends a warning but will continue to execute. 
+
+###
+TO RUN
+###
+This program accepts 0-1 arguments, if 0 arguments are provided, then 6 videos will be downloaded. If 1 argument is provided, its value can be numeric, or the string all. Below there are 3 examples where the first downloads all, the second downloads 2, the third downloads 6. 
+	python3 script.py all
+	python3 script.py 2
+	python3 script.py 
+
+###
 SERVER DOWNLOADING
 ###
 
--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import json, os, re
+import json, os, re, sys
 from termcolor import colored
 
 def fix_characters(title):
@@ -50,33 +50,50 @@ def check_excluded_list(excluded_vids, title):
     return True
   return False
 
-def download_videos():
+def download_videos(download_limit=6):
   """
   Downloads 6 videos at a time and outputs into the videos/ directory. 
   """
   videos = []
   for fname in os.listdir('yt_api_data'):
     videos += load_video_data(fname)
-  vids_downloaded = 1
+  vids_downloaded = 0
   excluded_vids = get_excluded_videos()
   for video_id, title in videos:
-    if vids_downloaded > 6:
-        break
+    if download_limit != 'all' and vids_downloaded == download_limit:
+      break
     title = title.replace(' ', '_')
     mkv_path = "videos/" + title + ".mkv"
     mp4_path = "videos/" + title + ".mp4"
     download_fpath = "videos/" + title
     if not check_excluded_list(excluded_vids, title) and not os.path.isfile(mkv_path) and not os.path.isfile(mp4_path):
-      print(colored(str(vids_downloaded) + ": ", "yellow") + colored(video_id + " downloading: " + download_fpath, "green"))
+      print(colored(str(vids_downloaded + 1) + ": ", "yellow") + colored(video_id + " downloading: " + download_fpath, "green"))
       command_prefix = "youtube-dl -o " + download_fpath
-      if video_id[0] == '-': 
-        os.system(command_prefix + " -- " + video_id)
-      else:
-        os.system(command_prefix + " " + video_id)
+      # if video_id[0] == '-': 
+      #   os.system(command_prefix + " -- " + video_id)
+      # else:
+      #   os.system(command_prefix + " " + video_id)
       vids_downloaded += 1
     else:
       print(colored("skipping download: " + title + "with youtube_id: " + video_id, "yellow"))
 
+def helper_text():
+  print(colored("This program accepts 0-1 arguments, if 0 arguments are provided, then 6 videos will be downloaded. If 1 argument is provided, its value can be numeric, or the string all. Below there are 3 examples where the first downloads all, the second downloads 2, the third downloads 6", "yellow"))
+  print(colored("    Example: python3 script.py all"))
+  print(colored("    Example: python3 script.py 2"))
+  print(colored("    Example: python3 script.py"))
+
 if __name__ == "__main__":
-  download_videos()
+  if len(sys.argv) == 1:
+    download_videos(6)
+    sys.exit()
+  elif len(sys.argv) == 2:
+    if sys.argv[1] == 'all':
+      download_videos('all')
+      sys.exit()
+    elif sys.argv[1].isnumeric():
+      download_videos(int(sys.argv[1]))
+      sys.exit()
+  helper_text()
+  
 
