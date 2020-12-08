965ae584c1fc71f232ff0174f4320e5f61f3250e
Steven Ha <stevenha@berkeley.edu>
2020-12-08 18:18:03 +0000

    add more excluded vids and add comments on functions

--- script.py
+++ script.py
@@ -2,9 +2,19 @@ import json, os, re
 from termcolor import colored
 
 def fix_characters(title):
+  """
+  Fixes title so that youtube-dl can output a
+  passable filename. Does not support 
+  non numeric or non a-Z characters. 
+  """
   return re.sub('[^0-9a-zA-Z]+', ' ', title)
 
 def append_videos(videos_info):
+  """
+  Used in load_video data to help extract the 
+  correct info needed to download the video, 
+  which is the videoId and title. 
+  """
   videos = []
   for video in videos_info:
     if 'videoId' in video['id']:
@@ -12,6 +22,10 @@ def append_videos(videos_info):
   return videos
 
 def load_video_data(fpath):
+  """
+  Reads yt data api json files to extract important 
+  downloading data.
+  """
   videos = []
   with open(fpath) as f:
     d1 = json.load(f)['items']
@@ -19,17 +33,26 @@ def load_video_data(fpath):
   return videos
 
 def get_excluded_videos():
+  """
+  Returns the set of videos that should be excluded from
+  the download. Consists of filenames that have already
+  been downloaded. 
+  """
   excluded_list = open('exclude.txt', 'r')
   return set([line.strip() for line in excluded_list.readlines()])
 
 def check_excluded_list(excluded_vids, title):
+  """
+  A check downloading the video to see if the video 
+  belongs in the excluded list. 
+  """
   if title + ".mkv" in excluded_vids or title + ".mp4" in excluded_vids or title + ".webm" in excluded_vids:
     return True
   return False
 
 def download_videos():
   """
-  Put videos*.json into a input folder so its more reusable. 
+  Downloads 6 videos at a time and outputs into the videos/ directory. 
   """
   videos = []
   for fname in os.listdir('yt_api_data'):
